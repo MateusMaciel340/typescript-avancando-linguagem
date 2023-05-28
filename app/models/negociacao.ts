fd2d24e6170c06d1053e4c0f0a0a068915a6c9db
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-05-28 08:27:32 -0300

    feat: solução de view

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -2,16 +2,24 @@ export class Negociacao {
 
     constructor(
         private _data: Date,
-        private readonly quantidade: number,
-        private readonly valor: number
+        private _quantidade: number,
+        private _valor: number
     ) {}
 
     get volume(): number {
-        return this.quantidade * this.valor;
+        return this._quantidade * this._valor;
     }
 
     get data(): Date {
         const data = new Date(this._data.getTime());
         return data;
+    }
+
+    get quantidade(): number {
+        return this._quantidade;
+    }
+
+    get valor(): number {
+        return this._valor;
     }
 }
